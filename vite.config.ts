9043cf56b943ce404cadc828f32b5637c43dc7c2
jorgeandres6 <jorgeandres6@msn.com>
2025-12-11 22:15:18 -0500

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,25 +5,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-        allowedHosts: [
-          'localhost', // Often included by default
-          '127.0.0.1', // Often included by default
-          'garnet-eudiometrical-ayden.ngrok-free.dev' // <-- **ADD THIS LINE**
-        ]
-
-      },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        outDir: 'dist',
+        sourcemap: false
       }
     };
 });
